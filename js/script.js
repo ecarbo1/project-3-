db79cbe958aa16d71c112b414b7cb87e274a7a7d
ecarbo1 <78318096+ecarbo1@users.noreply.github.com>
2021-05-04 17:00:06 -0500

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var map;
+
+function initMap() { 
+map = new google.maps.Map(document.getElementById("map"), {
+	center: {lat: 47.8783, lng: -90.6295}, 
+	zoom: 10, 
+});
+
+const brownbag = new google.maps.Marker ({
+	position: {lat:41.9435028, lng: -87.6717093},
+	map,
+	animation: google.maps.Animation.DROP,
+	title: "Brown Bag",
+});
+
+const iit = new google.maps.Marker ({
+	position: {lat: 41.83429528028157, lng: -87.62756505022413},
+	map,
+	animation: google.maps.Animation.DROP,
+	title: "IIT", 
+});
+
+const: Noblestreet = new google.maps.Marker ({
+	position: {lat: 41.9000524, lng: -87.6628151}, 
+	map,
+	animation: google.maps.Animation.DROP,
+	title: "Noble Street College Prep"
+});
+} 
+
