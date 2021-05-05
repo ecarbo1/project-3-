8fbe379b9d5754dac919940d0fd0a3af25528ebd
ecarbo1 <78318096+ecarbo1@users.noreply.github.com>
2021-05-04 22:00:18 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 var map;
-<style>
-		#map {
-	  		height: 100%;
-		}
 
-		html, body {
-			height: 100%;
-			margin: 0;
-			padding: 0;
-		}
-	</style>
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
 const brownbag = new google.maps.Marker ({
 	position: {lat:41.9435028, lng: -87.6717093},
 	map,
